85bd797befdd7694c1b6ca11fcebea108a5c0dc6
NeuroDonu <nicrdonu@gmail.com>
2024-09-19 16:15:34 +0300

    fix...

--- portablesource/__main__.py
+++ portablesource/__main__.py
@@ -1,6 +1,9 @@
 from .install_from_source import installed
 from .downloader import download_extract_and_cleanup
 
-if __name__ == "__main__":
+def main():
     download_extract_and_cleanup()
     installed()
+
+if __name__ == "__main__":
+    main()
